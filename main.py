5ca17dbb0a10b3e7d8d30d094a27ae8bc1b28007
a1u1 <46385440+a1u1@users.noreply.github.com>
2019-01-05 13:46:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,116 @@
+import requests
 
-import os
-import telebot
-
-
-token = "718226253:AAHHiFDewZ82_dolZVVaT53lbIwwA7RniEk"
-bot = telebot.TeleBot(token)
-APP_NAME = 'App1928'
+url = "https://api.telegram.org/bot718226253:AAHHiFDewZ82_dolZVVaT53lbIwwA7RniEk/"
 
 
-
-print(bot.get_me())
-
-def log(message):
-    print("\n ----------")
-    from datetime import datetime
-    print (datetime.now())
-    print ("Сообщение от {0} {1}. (id) = {2}) \n Текст - {3}".format(message.from_user.first_name,
-                                                             message.from_user.last_name,
-                                                             str(message.from_user.id),
-                                                             message.text))
-    #print(answer)
-
-user_a = "93672279"
-user_1 = "93672279"
-user_2 = "684010983"
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
 
 
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+
+send_mess(chat_id, 'Your message goes here')
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+    sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+def get_updates_json(request):
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
+    return response.json()
 
 
-
-# User 1
-
-    if str(message.from_user.id) == user_1:
-        answer = message.text
-        bot.send_message(user_2, answer)
-        log(message)
-        import datetime
-        a_answer = "Сообщение от {0} \n --------- \n {1} \n --------- \n {2}".format(message.from_user.first_name,message.text,
-                                                     f"{datetime.datetime.now():%Y-%m-%d %H:%M%:%S}")
-        bot.send_message(user_a, a_answer)
+import requests
+import datetime
 
 
-# User 2
+class BotHandler:
 
-    if str(message.from_user.id) == user_2:
-        answer = message.text
-        bot.send_message(user_1, answer)
-        log(message)
-        import datetime
-        a_answer = "Сообщение от {0} \n --------- \n {1} \n --------- \n {2}".format(message.from_user.first_name, message.text,
-                                                      f"{datetime.datetime.now():%Y-%m-%d %H:%M%:%S}")
-        bot.send_message(user_a, a_answer)
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
 
-bot.polling(none_stop=True, interval=0)
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
+
+    greet_bot = BotHandler(token)
+    greetings = ('hello', 'hi', 'greetings', 'sup')
+    now = datetime.datetime.now()
+
+    def main():
+        new_offset = None
+        today = now.day
+        hour = now.hour
+
+        while True:
+            greet_bot.get_updates(new_offset)
+
+            last_update = greet_bot.get_last_update()
+
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+
+            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+                greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+                greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+                greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+                today += 1
+
+            new_offset = last_update_id + 1
+
+    if __name__ == '__main__':
+        try:
+            main()
+        except KeyboardInterrupt:
+            exit()
